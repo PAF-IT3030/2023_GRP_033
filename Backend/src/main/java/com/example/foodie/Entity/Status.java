0a5fbcb53c1bc55bdf9c52e4a63923771ab6f453
Dinu Yasadi Masakorala <101055306+IT20301804@users.noreply.github.com>
2023-05-07 22:19:33 +0530

    Update Status Entity

--- Backend/src/main/java/com/example/foodie/Entity/Status.java
+++ Backend/src/main/java/com/example/foodie/Entity/Status.java
@@ -5,7 +5,7 @@ import java.sql.Timestamp;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Table ;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 @Table("Status")
 public class Status {

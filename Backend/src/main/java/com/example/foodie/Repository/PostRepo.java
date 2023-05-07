ae2c5a85b05c2ad37af54e6829e73d013032be59
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-07 21:06:51 +0530

    Make post repo

--- Backend/src/main/java/com/example/foodie/Repository/PostRepo.java
+++ Backend/src/main/java/com/example/foodie/Repository/PostRepo.java
@@ -1,5 +1,18 @@
 package com.example.foodie.Repository;
 
-public class PostRepo {
-    
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.example.foodie.Entity.Post;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepo extends CassandraRepository<Post, UUID> {
+
+	ArrayList<Post> findAll();
+	Post save(Post post);
+	void deleteById(UUID postID);
+	
 }

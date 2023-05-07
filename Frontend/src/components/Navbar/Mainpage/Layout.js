5a0afa1adf98f86434ae7ae1a7dcc8bb1ab13078
Ashen2020 <it20603472@my.sliit.lk>
2023-05-07 17:41:58 +0530

    home page ui updates

--- Frontend/src/components/Navbar/Mainpage/Layout.js
+++ Frontend/src/components/Navbar/Mainpage/Layout.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import "./Mainpage.css"
 
-class  extends Component {
+class  Layout extends Component {
     constructor(props) {
         super(props);
     }
     state = {  }
     render() { 
-        <div>
+        return (
+            <div className="mainpage__container">
+            <Grid container >
+                <Grid item xs={3}>
+                    <LeftSide />
+                </Grid>
+                <Grid item xs={6} className="middleContainer">
 
+                    <StatusBar />
+                    <UploadSection update={this.letUpdate} />
+                    <PostContainer ref="child"/>
+                </Grid>
+                <Grid item xs={3}>
+                    <RightSide />
+                </Grid>
+            </Grid>
         </div>
+
+         );
     }
 }
  
-export default ;
+export default Layout;
---
+++ Frontend/src/components/Navbar/Mainpage/LeftSidePanel/LeftSide.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+ class LeftSide extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                right
+            </div>
+        );
+    }
+ }
+  
+ export default LeftSide;

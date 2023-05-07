14ff2a358b8daf3ac14975360a9e6fff1f7b8f1e
Ashen2020 <it20603472@my.sliit.lk>
2023-05-07 11:05:57 +0530

    main page layout

---
+++ Frontend/src/components/Navbar/Mainpage/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Mainpage.css"
+
+class  extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        <div>
+
+        </div>
+    }
+}
+ 
+export default ;
